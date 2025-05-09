f2c418110efafe5f1065ebdb7f4782934c22c2e7
sarikap9 <sarika@platform9.com>
2025-05-10 00:16:04 +0530

    aa

--- scripts/generate_all_openapi.sh
+++ scripts/generate_all_openapi.sh
@@ -7,6 +7,7 @@ CRD_DIR="k8s/migration/config/crd/bases"
 ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 for TAG in $(git tag --sort=-creatordate); do
+  git stash --include-untracked
   git checkout $TAG
   if [ -d "$CRD_DIR" ] && [ "$(ls -A $CRD_DIR/*.yaml 2>/dev/null)" ]; then
     echo "$TAG: Folder exists and has YAML files."
