e015669d98ec9d652fb1e43821f70af402d78079
sarikap9 <sarika@platform9.com>
2025-05-23 04:07:52 +0530

    addded in astro

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -55,6 +55,7 @@ export default defineConfig({
 						{ label: 'Building', slug: 'guides/building' },
 						{ label: 'Using APIs', slug: 'guides/using_apis' },
 						{ label: 'Injecting Custom Environment Variables', slug: 'guides/injecting_custom_env' },
+						{ label: 'vJailbreak Scale Numbers', slug: 'guides/vJailbreak_scale_numbers' },
 					],
 				},
 				{
